ccdce7a38de131d672a24f6e60672cfbdacc5922
AkshatGarg22 <56624385+AkshatGarg22@users.noreply.github.com>
2019-10-16 12:19:25 +0530

    Update routes.py

--- routes/routes.py
+++ routes/routes.py
@@ -2,9 +2,12 @@ from flask import Blueprint
 
 router = Blueprint("router", __name__)
 
-@router.route("/check")
+@router.route("/hello")
 def check():
     return "Congratulations! Your app works. :)"
+@router.route("/hello")
+def hello():
+    return "it does!!!!. :)"
 
 @router.route("/add", methods=["POST"])
 def add():
